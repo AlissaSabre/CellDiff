3ced29e52a6c422586c100da37a203ef7ddef4ff
alissa <alissa_sabre@yahoo.co.jp>
2014-07-26 17:28:16 +0900

    Selected Ranges

---
+++ CellDiff/ExtendedMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NetOffice.ExcelApi;
+using NetOffice.ExcelApi.Enums;
+
+namespace CellDiff
+{
+    public static class ExtendedMethods
+    {
+        private static readonly char[] R1C1_DELIMITERS = "RC:".ToCharArray();
+
+        /// <summary>
+        /// Gets location values of a Range.
+        /// </summary>
+        /// <param name="range">The range to get the location.</param>
+        /// <returns>An array of four integer values to specify a location.</returns>
+        /// <remarks>
+        /// <para>
+        /// This method assumes the <paramref name="range"/> specifies a contiguous Range,
+        /// i.e., a single rectangular area of cells.
+        /// Otherwise the behaviour is undefined.
+        /// </para>
+        /// <para>
+        /// This method returns an array of four values, <c>{ c, r, w, h }</c>, where
+        /// <ul>
+        /// <li>c is the column address of the left-most cells, </li>
+        /// <li>r is the row address of the top-most cells, </li>
+        /// <li>w is the number of columns the range spans, and</li>
+        /// <li>h is the number of rows the range spans.</li>
+        /// </ul>
+        /// </para>
+        /// <para>
+        /// For example, 
+        /// given a range consisting of the top-left cell ("A1") on a worksheet,
+        /// this method will return <c>{ 1, 1, 1, 1 }</c>
+        /// </para>
+        /// </remarks>
+        public static int[] GetLocation(this Range range)
+        {
+            var a = range.Address(false, false, XlReferenceStyle.xlR1C1)
+                .Split(R1C1_DELIMITERS, 4, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => Int32.Parse(s)).ToArray();
+            return new[] { a[1], a[0], a[3] - a[1] + 1, a[2] - a[0] + 1 };
+        }
+    }
+}
--- CellDiff/Logic.cs
+++ CellDiff/Logic.cs
@@ -16,22 +16,70 @@ namespace CellDiff
             var selection = excel.Selection as Range;
             if (selection == null)
             {
-                MessageBox.Show("Please select cells to compare.");
+                Error("Please select cells to compare.");
                 return;
             }
+
+            Range range1, range2;
             switch (selection.Areas.Count)
             {
                 case 1:
-                    var area = selection.Areas[1]; // index is base 1
-                    var address = area.Address(false, false);
-                    MessageBox.Show(address);
+                    var area = selection.Areas[1];
+                    var loc = area.GetLocation();
+                    if (loc[2] == 2)
+                    {
+                        // Go vertical
+                        range1 = area.Columns[1];
+                        range2 = area.Columns[2];
+                    }
+                    else if (loc[3] == 2)
+                    {
+                        // GO horizontal
+                        range1 = area.Rows[1];
+                        range2 = area.Rows[2];
+                    }
+                    else
+                    {
+                        Error("WRONG SELECTION");
+                        return;
+                    }
                     break;
+
                 case 2:
+                    var area1 = selection.Areas[1];
+                    var area2 = selection.Areas[2];
+                    var loc1 = area1.GetLocation();
+                    var loc2 = area2.GetLocation();
+                    if ((loc1[2] != 1 && loc1[3] != 1) || (loc2[2] != 1 && loc2[3] != 1))
+                    {
+                        Error("WRONG SELECTION");
+                        return;
+                    }
+                    else if (loc1[2] == 1 && loc2[2] == 1 && loc1[3] == loc2[3]
+                        ||   loc1[2] == 1 && loc2[3] == 1 && loc1[3] == loc2[2]
+                        ||   loc1[3] == 1 && loc2[2] == 1 && loc1[2] == loc2[3]
+                        ||   loc1[3] == 1 && loc2[3] == 1 && loc1[2] == loc2[2])
+                    {
+                        range1 = area1;
+                        range2 = area2;
+                    }
+                    else
+                    {
+                        Error("When selecting two separate ranges, they must have a same length.");
+                        return;
+                    }
                     break;
                 default:
-                    MessageBox.Show("Please select either two contiguous ranges of cells or a contiguous range of cells.");
-                    break;
+                    MessageBox.Show("WRONG SELECTION");
+                    return;
             }
+
+            MessageBox.Show("Range1 = " + range1.Address + ", Range2 = " + range2.Address);
+        }
+
+        public static void Error(string s)
+        {
+            MessageBox.Show(s);
         }
     }
 }
